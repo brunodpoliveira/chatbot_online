a1e98cbca0e5e3f5a26854b13e29cb38c1de1344
Bruno Daniel <70975013+brunodpoliveira@users.noreply.github.com>
2020-11-04 18:14:36 -0300

    Logic adapters
    Updating Logic Adapters

--- test-algos/chatbot_test.py
+++ test-algos/chatbot_test.py
@@ -4,17 +4,29 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.response_selection import get_random_response
 from flask import Flask, render_template, request
 import spacy
-
 spacy.load('en')
+
+# CAUTION: REMOVE ALL LOGIC ADAPTERS EXCEPT import_path IF USING 'pt'
+# spacy.load('pt')
 # --------------------------------------
 # Instantiate agent
 # BestMatch = selects response by best known match to statement
 # response_selection_method has 3 options:
 # get_first_response,get_most_frequent_response or get_random_response
+# statement_comparison_function has 4 options:
+# read options and explanations here: https://chatterbot.readthedocs.io/en/stable/comparisons.html#statement-comparison
+# default_response is a response in case the bot doesn't know what to say
+# maximum_similarity_threshold:
+# maximum amount of similarity between 2 statement required before the search process is halted(default:0.95)
 bot = ChatBot('chatbot_test',
               read_only=False,
               response_selection_method=get_random_response,
-              logic_adapters=[{'import_path': 'chatterbot.logic.BestMatch'}],
+              logic_adapters=[{'import_path': 'chatterbot.logic.BestMatch',
+                               "response_selection_method": 'chatterbot.response_selection.get_random_response',
+                               "statement_comparison_function": 'chatterbot.comparisons.SynsetDistance',
+                               'default_response': 'Ainda não sei responder essa frase.',
+                               'maximum_similarity_threshold': 0.90
+                               }],
               storage_adapter='chatterbot.storage.SQLStorageAdapter',
               database='test1.sqlite3')
 
@@ -31,7 +43,7 @@ list_trainer = ListTrainer(bot)
 # Corpus Trainer
 corpus_trainer = ChatterBotCorpusTrainer(bot)
 
-# corpus_trainer.train('data/greetings.yml')
+# corpus_trainer.train('data/unilab.yml')
 
 
 corpus_trainer.train('data/botprofile.yml', 'data/compliment.yml', 'data/computers.yml', 'data/context_free_br.yml',
@@ -50,10 +62,10 @@ def conversation():
             question = input('Usuário: ')
             answer = bot.get_response(question)
             if answer.confidence > 0.5:
-                print('chatbot_test: ', answer)
+                print('bot: ', answer)
                 print('confidence', answer.confidence)
             else:
-                print('chatbot_test: ', 'Ainda não sei responder essa frase')
+                print('bot: ', 'Ainda não sei responder essa frase')
                 print('confidence', answer.confidence)
         # CONTROL+D to exit
         except (KeyboardInterrupt, EOFError, SystemExit):
