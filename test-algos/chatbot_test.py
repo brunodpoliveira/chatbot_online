5457079286bec4b0b201baef2f94d326c84770a8
Bruno <brunodp.oliveira@gmail.com>
2020-11-06 11:47:59 -0300

    Corrections + database
    Uploading the database, plus some minor tweaks to the site

--- test-algos/chatbot_test.py
+++ test-algos/chatbot_test.py
@@ -4,6 +4,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.response_selection import get_random_response
 from flask import Flask, render_template, request
 import spacy
+
 spacy.load('pt_core_news_sm')
 # --------------------------------------
 # Instantiate agent
@@ -40,15 +41,15 @@ list_trainer = ListTrainer(bot)
 # Corpus Trainer
 corpus_trainer = ChatterBotCorpusTrainer(bot)
 
-corpus_trainer.train('data/greetings.yml')
+# corpus_trainer.train('data/greetings.yml')
 
 
-#corpus_trainer.train('data/botprofile.yml', 'data/compliment.yml', 'data/computers.yml', 'data/context_free_br.yml',
-#                     'data/conversations.yml', 'data/emotion.yml', 'data/food.yml', 'data/games.yml',
-#                     'data/gossip.yml', 'data/greetings.yml', 'data/health.yml', 'data/history.yml',
-#                     'data/linguistic_knowledge.yml', 'data/literature.yml', 'data/money.yml', 'data/movies.yml',
-#                     'data/politics.yml', 'data/proverbs.yml', 'data/psychology.yml', 'data/science.yml',
-#                     'data/sports.yml', 'data/suggestions.yml', 'data/trivia.yml', 'data/unilab.yml')
+corpus_trainer.train('data/botprofile.yml', 'data/compliment.yml', 'data/computers.yml', 'data/context_free_br.yml',
+                     'data/conversations.yml', 'data/emotion.yml', 'data/food.yml', 'data/games.yml',
+                     'data/gossip.yml', 'data/greetings.yml', 'data/health.yml', 'data/history.yml',
+                     'data/linguistic_knowledge.yml', 'data/literature.yml', 'data/money.yml', 'data/movies.yml',
+                     'data/politics.yml', 'data/proverbs.yml', 'data/psychology.yml', 'data/science.yml',
+                     'data/sports.yml', 'data/suggestions.yml', 'data/trivia.yml', 'data/unilab.yml')
 
 
 # --------------------------------------
